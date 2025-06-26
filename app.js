bdb0c9be396e2d1df3b94fc973a39bed8028949b
joyjos <enjoyjos@hotmail.com>
2025-06-26 19:41:54 +0200

    Prompts to generate the diet

--- app.js
+++ app.js
@@ -21,7 +21,45 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 });
 
-// Guardo temporalmente los datos del usuario
+// Método para hacer petición a la IA
+const generateDiet = async(userResponses) => {
+
+    // Crear el prompt (sistema, indicaciones para la IA)
+    const promptSystem = {
+        role: "system",
+        content: `Eres un nutricionista profesional y un asistente que ayuda a 
+                  generar una dieta semanal.
+
+                  El usuario solo puede hacer preguntas relacionadas con la dieta
+                  con su peso, altura. objetivo, alergias, alimentos que no le gustan
+                  y número de comidas diarias.
+                  
+                  El sistema no responderá a ningún otro tipo de solicitud que no esté
+                  relacionada con la dieta.
+                  `
+    };
+
+    // Crear el prompt del usuario con la petición
+    const promptUser = {
+        role: "user",
+        content: `Crear una dieta semanal para una persona que pesa ${userResponses.peso} kg,
+                  mide ${userResponses.altura} cm, y cuyo objetivo es ${userResponses.objetivo}.
+                  La persona tiene las siguientes alergias: ${userResponses.alergias}.
+                  Evitar los siguientes alimentos: ${userResponses.no_gusta}.
+                  La persona quiere hacer ${userResponses.comidas_diarias} comidas diarias.
+
+                  Devualve la dieta en formato tabla markdown con las siguientes columnas:
+                  Día, Comida, Alimentos, Nombre del plato o receta, Calorías.
+                  Y no digas nda más, solo devuelve la tabla.
+        `
+    };
+
+    // Hacer petición a LLM de OpenAI
+
+    // Devolvemos el resultado generado
+}
+
+// Guardo temporalmente los datos del usuario (objeto de almacenamiento temporal de respuestas del usuario)
 let userData = {};
 
 // Ruta/endpoint/url
@@ -76,13 +114,10 @@ app.post("/api/nutri-chat", async(req, res) => {
         return res.json({reply: "Cuántas comidas quieres hacer cada día?"})
 
         // Ejecutar la petición a al IA con un prompt
+        const diet = await generateDiet(userData[userId]);
 
-
-        // Recoger la respuesta y darle la dieta al usuario
-
-
-        // Devolver respuesta
-        return res.json({reply: "¡Aquí tienes tu dieta!"});
+        // Devolver respuesta y la dieta
+        return res.json({reply: `¡Aquí tienes tu dieta! \n ${diet}`});
     }
 
     if(userData[userId].peso && userData[userId].altura && 
